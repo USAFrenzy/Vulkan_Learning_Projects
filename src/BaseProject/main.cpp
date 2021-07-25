d8d77178e5755586228b306bc1c36375ec389db9
Ryan McCullough <mccullough.ryan123@gmail.com>
2021-07-25 14:18:07 -0400

    Just a small update from home before work

---
+++ src/BaseProject/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+int main( )
+{
+	std::cout << "Testing Include headers allowing for compilation\n";
+	
+	std::getchar();
+}
