1b9f5babcc05061bf8a38720e791060364a65999
Ryan McCullough <mccullough.ryan123@gmail.com>
2021-07-28 23:32:45 -0400

    Removed the test_build dir

--- test_build/src/BaseProject/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Dev/Learning Projects/Vulkan_Learning_Projects/src/BaseProject
-# Build directory: E:/Dev/Learning Projects/Vulkan_Learning_Projects/test_build/src/BaseProject
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
